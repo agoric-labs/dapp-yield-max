585319337f575927b63fdfd47d19b12e7340efbf
Anthony <mandelliant@gmail.com>
2025-05-03 01:29:04 -0400

    refresh rates button

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -173,6 +173,12 @@ function App() {
                   <div className="max-yield">MAX YIELD: 2.83%</div>
                 </div>
               </div>
+              <button className="refresh-rates-button">
+                <svg className="lightning-icon" viewBox="0 0 24 24">
+                  <path d="M11 21h-1l1-7H7.5c-.58 0-.57-.32-.38-.66.19-.34.05-.08.07-.12C8.48 10.94 10.42 7.54 13 3h1l-1 7h3.5c.49 0 .56.33.47.51l-.07.15C12.96 17.55 11 21 11 21z"/>
+                </svg>
+                Refresh Rates
+              </button>
             </div>
 
             <div className="content-card">
